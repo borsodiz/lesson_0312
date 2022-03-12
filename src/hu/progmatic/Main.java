76949948f3a1adbe14efb16b9db5ae13ddc6fa55
borsodiz <zita_borsodi@yahoo.com>
2022-03-12 15:05:22 +0100

    First task

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,8 +1,22 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hali!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adj meg egy évet:");
+
+        int year = scanner.nextInt();
+
+        if ((year > 1582) && (year % 4 == 0)) {
+            if ((year % 100 != 0) || (year % 400 == 0)) {
+
+                System.out.println("Ez szökőév.");
+
+            }
+        }System.out.println("Nem szökőév.");
     }
 }
